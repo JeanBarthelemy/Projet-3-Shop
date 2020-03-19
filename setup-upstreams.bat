a9d4cef249d06f1c0c1f7ec7cef95a3ce1e49195
PREVOST Corentin <corentin.prevost@wildcodeschool.fr>
2020-03-19 17:19:51 +0100

    [+] Set up upstreams init script

---
+++ setup-upstreams.bat
@@ -0,0 +1,2 @@
+git remote set-url --push --add origin ssh://git@github.com/JeanBarthelemy/Projet-3-Shop
+git remote set-url --push --add origin ssh://git@gitlab.com/prevostcorentin/jeel-continuous-integration
